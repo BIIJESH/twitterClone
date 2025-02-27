351f26d0ede2015f917b9cb03f0cd10c050223ac
BIIJESH <BIIJESH@proton.me>
2025-02-27 23:11:33 +0545

    Initial Commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,13 @@
+import mongoose, { Mongoose } from "mongoose"
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  }
+})
