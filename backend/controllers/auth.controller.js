f576dc7580bcedb477c16e20bf49ba045f694caa
BIIJESH <BIIJESH@proton.me>
2025-02-27 23:59:35 +0545

    fix:minor import fix

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js"
-import generateTokenAndSetCookie from "../lib/utils/generateToken.js"
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
