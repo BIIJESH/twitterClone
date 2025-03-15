11818f2e7b210d6c30cf38484089136ce3325c75
BIIJESH <BIIJESH@proton.me>
2025-03-15 20:53:38 +0545

    add:getMe routes and proteced authorization

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectDb.js"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -9,6 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.json()) //regular function that runs between req and response // to parse req.body // can't use req.body with out this middleware parser
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
