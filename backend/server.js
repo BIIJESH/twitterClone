537c4b1db6ded794b09793d12becd75bb7181a49
BIIJESH <BIIJESH@proton.me>
2025-03-15 21:29:26 +0545

    fix:getUserProfile controller

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/connectDb.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -8,17 +9,16 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
 app.use(express.json()) //regular function that runs between req and response // to parse req.body // can't use req.body with out this middleware parser
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true })) //to parse data from(urlencoded)
+app.use(cookieParser())  //parse cookie
+
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Sever is running on port ${PORT}`)
   connectMongoDB()
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is ready")
-})
-//TODO:test signup endpoint tommorow
