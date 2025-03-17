d7958ddee1f7441957eada7c8572be343cbcba36
BIIJESH <BIIJESH@proton.me>
2025-03-17 13:07:22 +0545

    add:notifications sent to the user

---
+++ backend/models/notification.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+const notificationsSchema = new mongoose.Schema({
+  from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  to: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['follow', 'like']//add comment in here
+  },
+  read: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true })
+const Notification = mongoose.model('Notification', notificationsSchema)
+
+export default Notification
+
+//TODO:add comment notification
