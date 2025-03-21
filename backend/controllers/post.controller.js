5f3e98fec48dec77e2dfe5d9e5b8f832dc9a396e
BIIJESH <BIIJESH@proton.me>
2025-03-21 23:28:20 +0545

    add:like unlinke endpoint

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -95,6 +95,44 @@ export const commentOnPost = async (req, res) => {
     res.status(500).json({ error: "Internal server error" })
   }
 }
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+      const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+      return res.status(200).json(updatedLikes);
+    } else {
+      post.likes.push(userId);
+      await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+      await post.save();
+
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+
+      await notification.save();
+      const updatedLikes = post.likes;
+      return res.status(200).json(updatedLikes);
+    }
+  } catch (error) {
+    console.log("Error in like/unlike post:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 
 
