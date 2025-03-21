af4e816e8a6c6b7e3498710a933b816c3f636bb7
BIIJESH <BIIJESH@proton.me>
2025-03-22 00:05:47 +0545

    add:getting all post in sorted order

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -134,6 +134,28 @@ export const likeUnlikePost = async (req, res) => {
   }
 };
 
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password"
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password"
+      })
+    if (posts.length === 0) {
+      return res.status(200).json([])
+    }
+    return res.status(200).json(posts)
+  } catch (error) {
+    console.log("Error in getAll post", error)
+    return res.status(500).json({ error: "Internal Server Error" })
+  }
+}
+
 
 
 
