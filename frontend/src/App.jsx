e2dd0e5758ccf529055829425d96e16d42aeac3b
BIIJESH <BIIJESH@proton.me>
2025-04-16 11:19:23 +0545

    add:some dummy data and sidebars

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/auth/login/LoginPage'
 import SignUpPage from './pages/auth/signup/SignUpPage'
 import HomePage from './pages/home/HomePage'
+import Sidebar from './components/common/Sidebar'
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
