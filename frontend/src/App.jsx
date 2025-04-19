49004621ffb31a7cdf9aa8a32e9dbe1c41eaa2b9
BIIJESH <BIIJESH@proton.me>
2025-04-19 20:57:43 +0545

    add:logout and protected routes endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,29 +9,53 @@ import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-     return (
-          <div className="flex max-w-6xl mx-auto">
-               {/* this is a common component becaues this is out side of the routes */}
-               <Sidebar />
-               <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignUpPage />} />
-                    <Route
-                         path="/notifications"
-                         element={<NotificationPage />}
-                    />
-                    <Route
-                         path="/profile/:username"
-                         element={<ProfilePage />}
-                    />
-               </Routes>
-               <RightPanel />
-               <Toaster />
-          </div>
-     );
+  const { data, isLoading } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me")
+        const data = await res.json()
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+      } catch (error) {
+        console.log(error)
+        throw Error
+      }
+    }
+  })
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    )
+  }
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      {/* this is a common component becaues this is out side of the routes */}
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/notifications"
+          element={<NotificationPage />}
+        />
+        <Route
+          path="/profile/:username"
+          element={<ProfilePage />}
+        />
+      </Routes>
+      <RightPanel />
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,38 +3,22 @@ import daisyUIThemes from "daisyui/theme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-     theme: {
-          extend: {
-               colors: {
-                    blue: "#1DA1F2",
-                    blueDark: "#1A91DA",
-                    blueDarker: "#1681BF",
-                    black: "#14171A",
-                    darkGray: "#657786",
-                    lightGray: "#AAB8C2",
-                    xlightGray: "#E1E8ED",
-                    xxlightGray: "#F5F8FA",
-                    red: "#E0245E",
-                    green: "#17BF63",
-                    yellow: "#FFAD1F",
-                    purple: "#794BC4",
-                    orange: "#F45D22",
-               },
-          },
-     },
-     plugins: [daisyui],
-     daisyui: {
-          themes: [
-               "dark",
-               {
-                    black: {
-                         ...daisyUIThemes["black"],
-                         primary: "rgb(29, 155, 240)",
-                         secondary: "rgb(24, 24, 24)",
-                         //3 13 12
-                    },
-               },
-          ],
-     },
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
 };
